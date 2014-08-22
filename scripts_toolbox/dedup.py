0c351ca25ac1c4907562eca025448fa04146dc35
Rodrigo Garcia <rgarcia@inmegen.gob.mx>
2014-08-22 18:40:16 -0500

    will swap axes, dedup and then unswap

--- scripts_toolbox/dedup.py
+++ scripts_toolbox/dedup.py
@@ -1,25 +1,31 @@
 import argparse
+import numpy as np
+import pprint
 
 parser = argparse.ArgumentParser(description='will remove lines with duplicate data regardless of key')
-parser.add_argument('--input', type=argparse.FileType('r'), required=True, 
-                   help='file to process')
+parser.add_argument('--input', type=argparse.FileType('r'), required=True, help='file to process')
 
 args = parser.parse_args()
 
 lineas = args.input.readlines()
 
-
-p = {}
-
+arreglote = []
 for l in lineas:
-    campos = l.split()
-    sample = campos[0]
-    datos  = tuple(campos[1:])
-    if datos in p:
-        p[datos].append(sample)
-    else:
-        p[datos]=[sample,]
+    columnas = l.split()
+    arreglote.append(columnas)
 
+
+swapped = np.array(arreglote).swapaxes(1,0)
+
+# count duplicate keys for same sets of values
+p = {}
+for campos in swapped:
+    sample = campos[0]
+    data  = tuple(campos[1:])
+    if data in p:
+        p[data].append(sample)
+    else:
+        p[data]=[sample,]
 
 
 redundantes = []
@@ -27,9 +33,14 @@ for datos in p:
     if len(p[datos])>1:
         redundantes+=p[datos][1:]
 
-for l in lineas:
-    campos = l.split()
-    sample = campos[0]
-    if not sample in redundantes:
-        print l.strip()
 
+tmp = []
+for l in swapped:
+    sample = l[0]
+    if not sample in redundantes:
+        tmp.append(l)
+    
+unswapped = np.array(tmp).swapaxes(1,0)
+
+for l in unswapped:
+    print '\t'.join(l)
